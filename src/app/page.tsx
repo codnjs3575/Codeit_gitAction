975581db6404b611d8fb3e7636e08fc66f2abe0d
“codnjs3575” <codnjs3575@naver.com>
2025-09-18 12:41:45 +0900

    vercel & action 연동 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,10 @@
 'use client';
 
 export default function Home() {
-  return <div>{process.env.NEXT_PUBLIC_API}</div>;
+  return (
+    <div>
+      <h1>게시글 목록</h1>
+      <div>{process.env.NEXT_PUBLIC_API}</div>
+    </div>
+  );
 }
