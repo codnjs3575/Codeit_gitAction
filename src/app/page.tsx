5560ac80e346c98d52b2c48d268e8aab88d5a65f
“codnjs3575” <codnjs3575@naver.com>
2025-09-18 09:33:01 +0900

    feat) lint 확인하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 // src/app/page.tsx
 
-"use client";
-import { Button } from "devcw-storybook-design-system";
+'use client';
+import { Button } from 'devcw-storybook-design-system';
 
 export default function Home() {
+  const a = 1;
+
   return (
     <div>
       <Button
         variant="primary"
         size="lg"
         onClick={() => {
-          alert("버튼 클릭");
+          alert('버튼 클릭');
         }}
       >
         안녕
